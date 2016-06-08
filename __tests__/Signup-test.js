776a3f2956da4e99d9c79525346cd02dad31939d
Jason Vanderslice <jlvanderslice@gmail.com>
2016-06-08 12:36:12 -0500

    Happy and unhappy path testing added

--- __tests__/Signup-test.js
+++ __tests__/Signup-test.js
@@ -4,14 +4,20 @@ import TestUtils from 'react-addons-test-utils';
 import Signup from '../modules/Signup';
 
 jest.unmock("../modules/Signup");
+jest.unmock("validator");
 
 describe('User signup app', () => {
-  var signupRendered;
+  var signupRendered,
+      e
 
   beforeEach(()=>{
     signupRendered = TestUtils.renderIntoDocument(
       <Signup/>
     );
+
+    e = {
+      preventDefault: ()=>{ }
+    };
   });
 
   it('contains username input', ()=> {
@@ -19,23 +25,42 @@ describe('User signup app', () => {
     expect(usernameInput).toBeDefined();
   });
 
-  it('contains password input', ()=> {
-    var passwordInput = TestUtils.findRenderedDOMComponentWithClass(signupRendered, "form__password");
-    expect(passwordInput).toBeDefined();
+  describe("tests input validity", ()=>{
+    beforeEach(()=>{
+      spyOn(signupRendered, "checkInputValidity").and.callThrough();
+    });
+
+    describe("username input is valid", ()=>{
+      var stubbedFormData = {
+          username: 'ted mosby'
+      }
+
+      it("should set state as true", ()=>{
+        signupRendered.checkInputValidity(stubbedFormData);
+        expect(signupRendered.state.isUsernameValid).toBe(true);
+      });
+    });
+
+    describe("username input is invalid", ()=>{
+      var stubbedFormData = {
+          username: 'te'
+      }
+
+      it("should set state as false", ()=>{
+        signupRendered.checkInputValidity(stubbedFormData);
+        expect(signupRendered.state.isUsernameValid).toBe(false);
+      });
+    });
   });
 
   describe("when signup form is submitted", ()=>{
-    var e;
-    
-    beforeEach(()=>{
-      e = {
-        preventDefault: ()=>{ }
-      };
 
+    beforeEach(()=>{
       spyOn(e, "preventDefault");
       spyOn(signupRendered, "handleUserSignup").and.callThrough();
       spyOn(signupRendered, "sendSignupRequest");
       spyOn(signupRendered, "serializeFormData");
+      spyOn(signupRendered, "checkInputValidity");
 
       signupRendered.handleUserSignup(e);
     });
@@ -50,6 +75,29 @@ describe('User signup app', () => {
 
     it("calls send signup request", ()=>{
       expect(signupRendered.sendSignupRequest).toBeCalled();
+    });
+  });
+
+  describe("When a user signs up with no information", ()=>{
+    beforeEach(()=>{
+      spyOn(e, "preventDefault");
+      spyOn(signupRendered, "handleUserSignup").and.callThrough();
+      spyOn(signupRendered, "sendSignupRequest");
+      spyOn(signupRendered, "serializeFormData");
+      spyOn(signupRendered, "checkInputValidity");
+      signupRendered.handleUserSignup(e);
+    });
+
+    it("should provide error text for username", ()=>{
+      var errorElement = TestUtils.findRenderedDOMComponentWithClass(signupRendered, "username__error");
+      signupRendered.setState({
+        isUsernameValid: false
+      });
+      expect(errorElement.textContent).toBe("Username must be 3 - 100 characters");
+    });
+
+    it("should not submit", ()=>{
+
     });
   });
 });
--- modules/Signup.js
+++ modules/Signup.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Router, browserHistory } from "react-router"
 import $ from 'jquery'
 import Serialize from 'form-serialize'
-import { Router, browserHistory } from "react-router"
+import Validator from 'validator'
 
 export default React.createClass({
   getInitialState(){
     return {
-      username: ''
+      username: '',
+      isUsernameValid: true
     }
   },
   handlePostSuccess(resp){
@@ -22,18 +24,26 @@ export default React.createClass({
   serializeFormData(form){
     return Serialize(form, {hash: true});
   },
+  checkInputValidity(formData){
+    this.setState({
+      isUsernameValid: Validator.isLength(formData.username, {min: 3, max: 100})
+    });
+  },
   handleUserSignup(e){
     e.preventDefault();
     var formData = this.serializeFormData(this.refs["form--signup"])
     this.sendSignupRequest(formData);
+    this.checkInputValidity(formData);
   },
   render() {
     return (
       <section>
         <h2 ref="username"> {this.state.username ? this.state.username : "not signed in"} </h2>
         <form method="POST" action="#signup" onSubmit={this.handleUserSignup} ref="form--signup" className="form--signup">
-          <input className="form__username" type="text" name="username" placeholder="username" />
-          <input className="form__password" type="text" name="password" placeholder="password" />
+          <h3 className="username__error">
+            { this.state.isUsernameValid ? '' : 'Username must be 3 - 100 characters' }
+          </h3>
+          <input className="form__username" type="text" name="username" placeholder="username" ref="form__username"/>
           <input type="submit" value="signup"/>
         </form>
       </section>
